427d2f67057462219b1caa65db2d576f1a14e0e5
Maycon Geovane <maycon.geovane.03@gmail.com>
2025-01-11 19:47:04 -0300

    definições e inclusões
    incluindo as bibliotecas, e definindo as constantes, principalmente as booleanas.

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <xinu.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PINO_MOTOR 5
+#define SENSOR_INICIO 6
+#define SENSOR_FIM 7
+#define ATRASO 500
+
+#define TRUE 1
+#define FALSE 0
